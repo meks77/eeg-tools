69e3ef595820f41b3a4e038a7c6558ec63572142
Markus Hager <meks77@gmail.com>
2026-04-01 15:28:22 +0200

    first simplest calculation
    But still some things need to be considered

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,19 +1,10 @@
 package at.hillstromeeg
 
-import org.jetbrains.compose.web.dom.Div
-import org.jetbrains.compose.web.dom.H1
-import org.jetbrains.compose.web.dom.Text
+import at.hillstromeeg.ui.CalculatorPage
 import org.jetbrains.compose.web.renderComposable
 
 fun main() {
     renderComposable(rootElementId = "root") {
-        Div {
-            H1 {
-                Text("Willkommen bei EEG Tools")
-            }
-            Div {
-                Text("Einfache Berechnung der Ersparnis für Verbraucher")
-            }
-        }
+        CalculatorPage()
     }
 }
